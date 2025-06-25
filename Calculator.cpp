ea01c9f455525c3e965a017ed46581986c58ce4c
IceMan <abriano600@gmail.com>
2025-06-25 09:57:19 -0500

    Refactored calculator code to read whole equations.

---
+++ Calculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    std::string Equation;
+
+    std::vector<char> Numbers(5);
+
+    std::cout << "Enter Equation";
+    std::cin >> Equation;
+    // std::cout << "Enter 2 numbers ";
+    // std::cin >> num1 >> num2;
+    // std::cout << num1, num2;
+
+    for (char Character : Equation)
+    {
+        std::count << Equation;
+    }
+    
+
+    return 0;
+}
