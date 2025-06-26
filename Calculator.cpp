98162c88368a7792502aad524cec48034c255d01
IceMan <abriano600@gmail.com>
2025-06-25 22:12:42 -0500

    Added multiple digit expression solving

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,45 +2,79 @@
 #include <vector>
 #include <string>
 
+std::string LeftNumber = "";
+std::string RightNumber = "";
+
+void ConnectNumsLeft(int StartIndex, std::string Expression)
+{
+    if (Expression[StartIndex - 1])
+    {
+        char NextChar = Expression[StartIndex - 1];
+        LeftNumber.insert(0, 1, NextChar);
+        ConnectNumsLeft(StartIndex - 1, Expression);
+    };
+}
+
+void ConnectNumsRight(int StartIndex, std::string Expression)
+{
+    if (Expression[StartIndex + 1])
+    {
+        char NextChar = Expression[StartIndex + 1];
+        RightNumber.insert(RightNumber.length(), 1, NextChar);
+        ConnectNumsRight(StartIndex + 1, Expression);
+    };
+}
+
 int main()
 {
     while (true)
     {
-        std::string Equation;
-        std::vector<char> Numbers(5);
-        std::cout << "Enter Equation: ";
-        std::cin >> Equation;
+        std::string Expression;
+        std::cout << "Enter Expression: ";
+        std::cin >> Expression;
         int Index = 0;
 
-        for (char Character : Equation)
+        for (char Character : Expression)
         {
+
             switch (Character)
             {
             case '*':
                 // std::cout << Index;
-                std::cout << (Equation[Index - 1] - '0') * (Equation[Index + 1] - '0') << std::endl;
+                ConnectNumsLeft(Index, Expression);
+                ConnectNumsRight(Index, Expression);
+
+                std::cout << std::stoi(LeftNumber) * std::stoi(RightNumber) << std::endl;
                 break;
             case '/':
-                // std::cout << Index;
-                std::cout << static_cast<double>((Equation[Index - 1] - '0')) / (Equation[Index + 1] - '0') << std::endl;
+                ConnectNumsLeft(Index, Expression);
+                ConnectNumsRight(Index, Expression);
+
+                std::cout << static_cast<double>((std::stoi(LeftNumber))) / std::stoi(RightNumber) << std::endl;
                 break;
             case '+':
-                // std::cout << Index;
-                std::cout << (Equation[Index - 1] - '0') + (Equation[Index + 1] - '0') << std::endl;
+                ConnectNumsLeft(Index, Expression);
+                ConnectNumsRight(Index, Expression);
+
+                std::cout << std::stoi(LeftNumber) + std::stoi(RightNumber) << std::endl;
                 break;
             case '-':
-                // std::cout << Index;
-                std::cout << (Equation[Index - 1] - '0') - (Equation[Index + 1] - '0') << std::endl;
+                ConnectNumsLeft(Index, Expression);
+                ConnectNumsRight(Index, Expression);
+
+                std::cout << std::stoi(LeftNumber) - std::stoi(RightNumber) << std::endl;
                 break;
             default:
-                // std::cout << "Invalid Symbol!" << std::endl;
                 break;
             }
 
             Index++;
         }
 
-        if (Equation != "exit")
+        LeftNumber = "";
+        RightNumber = "";
+
+        if (Expression != "exit")
         {
             continue;
         }
