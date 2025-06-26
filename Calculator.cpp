e4d63b67709af4a347fbd93077d7e4ff56e57254
IceMan <abriano600@gmail.com>
2025-06-26 15:13:05 -0500

    Added an array that stores functions that will be modified based on the equation

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
+#include <any>
+
 
 std::string LeftNumber = "";
 std::string RightNumber = "";
 
-void ConnectNumsLeft(int StartIndex, std::string Expression)
-{
-    if (Expression[StartIndex - 1])
-    {
-        char NextChar = Expression[StartIndex - 1];
-        LeftNumber.insert(0, 1, NextChar);
-        ConnectNumsLeft(StartIndex - 1, Expression);
-    };
+void Add(int a, int b) {
+
 }
 
-void ConnectNumsRight(int StartIndex, std::string Expression)
-{
-    if (Expression[StartIndex + 1])
-    {
-        char NextChar = Expression[StartIndex + 1];
-        RightNumber.insert(RightNumber.length(), 1, NextChar);
-        ConnectNumsRight(StartIndex + 1, Expression);
-    };
+void Subtract(int a, int b) {
+
+}
+
+void Multiply(int a, int b) {
+
+}
+
+void Divide (int a, int b) {
+
 }
 
 int main()
@@ -34,33 +33,30 @@ int main()
         std::cin >> Expression;
         int Index = 0;
 
+        std::vector<std::any> MyArray;
+
+
         for (char Character : Expression)
         {
 
             switch (Character)
             {
             case '*':
-                // std::cout << Index;
-                ConnectNumsLeft(Index, Expression);
-                ConnectNumsRight(Index, Expression);
 
                 std::cout << std::stoi(LeftNumber) * std::stoi(RightNumber) << std::endl;
                 break;
             case '/':
-                ConnectNumsLeft(Index, Expression);
-                ConnectNumsRight(Index, Expression);
+
 
                 std::cout << static_cast<double>((std::stoi(LeftNumber))) / std::stoi(RightNumber) << std::endl;
                 break;
             case '+':
-                ConnectNumsLeft(Index, Expression);
-                ConnectNumsRight(Index, Expression);
+
 
                 std::cout << std::stoi(LeftNumber) + std::stoi(RightNumber) << std::endl;
                 break;
             case '-':
-                ConnectNumsLeft(Index, Expression);
-                ConnectNumsRight(Index, Expression);
+
 
                 std::cout << std::stoi(LeftNumber) - std::stoi(RightNumber) << std::endl;
                 break;
