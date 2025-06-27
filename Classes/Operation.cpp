ea8530a6140da29a4e786ff7fafe39875382a410
IceMan <abriano600@gmail.com>
2025-06-27 15:08:52 -0500

    Added Operator Class and sequential evaluation

---
+++ Classes/Operation.cpp
@@ -0,0 +1,28 @@
+#include "Operation.h"
+#include <iostream>
+
+Operation::Operation(float a, float b, int Index, char Op)
+    : First(a), Second(b), Index(Index), Operator(Op)
+{
+
+}
+
+float Operation::Evaluate()
+{
+    std::cout << "Hello from MyClass!" << std::endl;
+    switch (Operator)
+    {
+    case '+':
+        return First + Second;
+        break;
+    case '-':
+
+        return First - Second;
+        break;
+
+    default:
+        std::cout << "Invalid Operator";
+        return 0.0f;
+        break;
+    }
+}
